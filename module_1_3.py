227c11dc074135d30627fbb4d72050f08df753ff
Meg09071969 <edgoodly@gmail.com>
2024-08-05 14:31:07 +0300

    Добавил файл module_1_3.py - Практическая работа по уроку "Динамическая типизация".

--- module_1_3.py
+++ module_1_3.py
@@ -1,2 +1,9 @@
-print('Hello World')
-print('Hello Eduard')
+name = "Eduard"
+print(name)
+age = 55
+print("Age: 55")
+new_age = 58
+print("New_Age: 58")
+Is_Student = 5 < 10
+print("Is_Student: True")
+
