d8b5da481690ecfe65569fbcf547e0d6cd4e9b60
Meg09071969 <edgoodly@gmail.com>
2024-08-05 15:11:45 +0300

    Добавил файл module_1_3.py - Практическая работа по уроку "Динамическая типизация". В 15.11 мск внёс исправления

--- module_1_3.py
+++ module_1_3.py
@@ -2,8 +2,8 @@ name = "Eduard"
 print(name)
 age = 55
 print("Age: 55")
-new_age = 58
+new_age = age + 3
 print("New_Age: 58")
-Is_Student = 5 < 10
-print("Is_Student: True")
+Is_Student = 55 < 58
+print("Is_Student: True")  # boolean
 
