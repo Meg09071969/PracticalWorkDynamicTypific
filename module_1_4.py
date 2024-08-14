130e8eb9329e9bc7ba9b87aaab1fb110976070da
Meg09071969 <edgoodly@gmail.com>
2024-08-14 19:16:25 +0300

    Добавил файл module_1_4.py - "Организация программ и методы строк".

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input("Студент университета Urban: ")
+print("Здравствуйте, ", my_string, "!")
+print(len(my_string))
+print(my_string. upper())
+print(my_string. lower())
+my_string = "Эдуард Маенков"
+print(my_string.replace(" ", "#" ))
+print(my_string[0])
+print(my_string[-1::])
+
+
+
