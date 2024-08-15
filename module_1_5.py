51b103db88f5d4fe069cb7b6f2843e1087c139c8
Meg09071969 <edgoodly@gmail.com>
2024-08-15 12:16:26 +0300

    Добавил файл module_1_5.py - "Неизменяемые и изменяемые объекты. Кортежи".

--- module_1_5.py
+++ module_1_5.py
@@ -1,3 +1,9 @@
 immutable_var = 1, 2, "E", "D"
 print(immutable_var)
+#immutable_var [0] = 200
+print(immutable_var ) # элементы кортежа изменить нельзя потому что кортеж является  неизменяемой коллекцией
 
+mutable_list = [9, 7, 1969, "Дата рождения"]
+print(mutable_list)
+mutable_list[1] = "июля"
+print(mutable_list)
