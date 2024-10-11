aa02ce78717dc4c04dc336fd65e06ee37939dcc8
Meg09071969 <edgoodly@gmail.com>
2024-10-12 00:08:01 +0300

    Задание по теме: Цикл For элементы списка

---
+++ module_2_4.py
@@ -0,0 +1,27 @@
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+print("numbers =", numbers)
+
+primes = []
+not_primes = []
+
+for number in numbers:
+    if number == 1:
+        continue
+
+    is_prime = True
+
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False  # Если нашёлся делитель, то число не простое
+            break  # Прерываем цикл, если нашли делитель
+
+    if is_prime:
+        primes.append(number)
+    else:
+        not_primes.append(number)
+
+print("Primes:", primes)
+print("Not Primes:", not_primes)
+
+
